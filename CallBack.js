07ccb8669c2cf2b565f193a59f5265f8b04aa677
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-23 14:22:27 -0300

    feat: other callback

--- CallBack.js
+++ CallBack.js
@@ -10,3 +10,29 @@ function cumprimentar(nome, indice) {
 
 nomes.forEach(cumprimentar, contexto);
 
+/* Callback */
+
+const medias = [5.5, 7.8, 6.2, 9.1, 4.3, 8.0];
+
+// Cálculo da média geral
+const mediaGeral = medias.reduce((acc, val) => acc + val, 0) / medias.length;
+
+// Sem callback
+const abaixoDaMedia1 = [];
+for (let i in medias) {
+  if (medias[i] < mediaGeral) {
+    abaixoDaMedia1.push(medias[i]);
+  }
+}
+console.log('Sem callback:', abaixoDaMedia1);
+
+// Com callback direto
+const abaixoDaMedia2 = medias.filter(function (valor) {
+  return valor < mediaGeral;
+});
+console.log('Com callback:', abaixoDaMedia2);
+
+// Com arrow + função externa
+const menorQueMedia = m => m < mediaGeral;
+const abaixoDaMedia3 = medias.filter(menorQueMedia);
+console.log('Com arrow:', abaixoDaMedia3);
