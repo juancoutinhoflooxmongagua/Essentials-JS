682157012d5f673658614f89d586f36ddd353a61
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-23 12:23:30 -0300

    feat: working with arrow functions

---
+++ ArrowFunction.js
@@ -0,0 +1,17 @@
+let dobro = function (a) {
+
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a
+console.log(dobro(Math.PI))
+
+let hello = function() {
+    return 'hELLO'
+}
+
+hello = () => 'Hello'
+hello = _ => 'Hello'
